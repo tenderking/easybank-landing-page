919c46dab23ebac6ea272a075302202ef3907cab
George <gmu003@gmail.com>
2022-09-12 18:32:10 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <h1 class="text-3xl text-purple-600 font-bold underline">Hello world!</h1>
+    </div>
+  </header>
+
+  <main></main>
+</template>
