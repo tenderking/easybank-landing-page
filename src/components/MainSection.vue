2956f03f9e0ab943d0bf65edb463257e8264fd4b
tenderking <gmu003@gmail.com>
2022-09-14 12:06:15 +0200

    refactor: assets must be import to work in build

--- src/components/MainSection.vue
+++ src/components/MainSection.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
 	import IconBg from "./icons/IconBg.vue";
 	import CtaButton from "./CtaButton.vue";
-
+	import banking from "@/assets/images/icon-online.svg";
+	import budgeting from "@/assets/images/icon-budgeting.svg";
+	import onboarding from "@/assets/images/icon-onboarding.svg";
+	import api from "@/assets/images/icon-api.svg";
 	interface Items {
 		icon: string;
 		title: string;
@@ -10,25 +13,25 @@
 
 	const items: Items[] = [
 		{
-			icon: "src/assets/images/icon-online.svg",
+			icon: banking,
 			title: "Online Banking",
 			description:
 				"Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world.",
 		},
 		{
-			icon: "src/assets/images/icon-budgeting.svg",
+			icon: budgeting,
 			title: "Simple Budgeting",
 			description:
 				"See exactly where your money goes each month. Receive notifications when you’re close to hitting your limits.",
 		},
 		{
-			icon: "src/assets/images/icon-onboarding.svg",
+			icon: onboarding,
 			title: "Fast Onboarding",
 			description:
 				"We don’t do branches. Open your account in minutes online and start taking control of your finances right away.",
 		},
 		{
-			icon: "src/assets/images/icon-api.svg",
+			icon: api,
 			title: "Open API",
 			description:
 				"Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier.",
